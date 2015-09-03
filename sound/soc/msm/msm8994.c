ff93384c47c9dc9c9548d20a72fdbab5afc652c3
Phani Kumar Uppalapati <phaniu@codeaurora.org>
2015-09-03 14:22:49 -0700

    ASoc: wcd: mbhc: Change the key code mapping of buttons
    Change the key code mapping of buttons as below
    according to Android L spec.
    
    Button0 0-70 ohm - hook key
    Button1 110-180 ohm - voice assist
    Button2 210-290 ohm - volume up
    Button3 360-680 ohm - volume down
    
    Change-Id: Ic2d79ad112a8cad25b6d0808c7dabf105113b4b5
    Signed-off-by: Phani Kumar Uppalapati <phaniu@codeaurora.org>
    
    Conflicts:
    	sound/soc/msm/msm8994.c

--- sound/soc/msm/msm8994.c
+++ sound/soc/msm/msm8994.c
@@ -19,6 +19,7 @@
 #include <linux/qpnp/clkdiv.h>
 #include <linux/regulator/consumer.h>
 #include <linux/io.h>
+#include <linux/input.h>
 #include <linux/module.h>
 #include <linux/switch.h>
 #include <sound/core.h>
@@ -214,6 +215,14 @@ static struct wcd9xxx_mbhc_config mbhc_cfg = {
 	.use_vddio_meas = true,
 	.enable_anc_mic_detect = false,
 	.hw_jack_type = FOUR_POLE_JACK,
+	.key_code[0] = KEY_MEDIA,
+	.key_code[1] = KEY_VOICECOMMAND,
+	.key_code[2] = KEY_VOLUMEUP,
+	.key_code[3] = KEY_VOLUMEDOWN,
+	.key_code[4] = 0,
+	.key_code[5] = 0,
+	.key_code[6] = 0,
+	.key_code[7] = 0,
 };
 #else
 static struct wcd9xxx_mbhc_config mbhc_cfg = {
@@ -236,6 +245,14 @@ static struct wcd9xxx_mbhc_config mbhc_cfg = {
 	.use_vddio_meas = true,
 	.enable_anc_mic_detect = false,
 	.hw_jack_type = SIX_POLE_JACK,
+	.key_code[0] = KEY_MEDIA,
+	.key_code[1] = KEY_VOICECOMMAND,
+	.key_code[2] = KEY_VOLUMEUP,
+	.key_code[3] = KEY_VOLUMEDOWN,
+	.key_code[4] = 0,
+	.key_code[5] = 0,
+	.key_code[6] = 0,
+	.key_code[7] = 0,
 };
 #endif
 
@@ -2824,21 +2841,21 @@ static void *def_codec_mbhc_cal(void)
 	btn_high = wcd9xxx_mbhc_cal_btn_det_mp(btn_cfg,
 					       MBHC_BTN_DET_V_BTN_HIGH);
 	btn_low[0] = -50;
-	btn_high[0] = 260;// 20;
-	btn_low[1] = 261;//21;
-	btn_high[1] = 270;//61;
-	btn_low[2] = 271;//62;
-	btn_high[2] = 300;//104;
-	btn_low[3] = 301;//105;
-	btn_high[3] = 330;//148;
-	btn_low[4] = 331;//149;
-	btn_high[4] = 360;//189;
-	btn_low[5] = 361;//190;
-	btn_high[5] = 390;//228;
-	btn_low[6] = 391;//229;
-	btn_high[6] = 420;//269;
-	btn_low[7] = 421;//270;
-	btn_high[7] = 500;//500;
+	btn_high[0] = 90;
+	btn_low[1] = 130;
+	btn_high[1] = 220;
+	btn_low[2] = 235;
+	btn_high[2] = 335;
+	btn_low[3] = 375;
+	btn_high[3] = 655;
+	btn_low[4] = 656;
+	btn_high[4] = 660;
+	btn_low[5] = 661;
+	btn_high[5] = 670;
+	btn_low[6] = 671;
+	btn_high[6] = 680;
+	btn_low[7] = 681;
+	btn_high[7] = 690;
 	n_ready = wcd9xxx_mbhc_cal_btn_det_mp(btn_cfg, MBHC_BTN_DET_N_READY);
 	n_ready[0] = 80;
 	n_ready[1] = 68;
