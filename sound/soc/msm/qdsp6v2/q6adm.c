999d8d07d932d43a835e40c10ce75b59268cfcdc
Matt Wagantall <mwagantall@cyngn.com>
2015-11-04 14:52:41 -0800

    ASoC: msm: qdsp6v2: Don't send TruMedia commands to DSP if COPP is resetting
    This avoids memory scribbling when update_params_ptr is written to
    in srs_trumedia_open() despite the memory it points to having already
    been freed.
    
    Although this scenario should not be allowed to arise in the first
    place, this added check avoids potential instability if it does.
    
    This avoids the scribbling crash reported in SAMBAR-988, for which a
    fix for the COPP reset is still under discussion.
    
    Change-Id: I70843144673ddf35650a6f5796f1f3baea633ca3
    Signed-off-by: Matt Wagantall <mwagantall@cyngn.com>

--- sound/soc/msm/qdsp6v2/q6adm.c
+++ sound/soc/msm/qdsp6v2/q6adm.c
@@ -446,6 +446,13 @@ int srs_trumedia_open(int port_id, int copp_idx, __s32 srs_tech_id,
 		pr_err("%s: Invalid port_id %#x\n", __func__, port_id);
 		return -EINVAL;
 	}
+
+	if (atomic_read(&this_adm.copp.id[port_idx][copp_idx]) == RESET_COPP_ID) {
+		pr_err("%s: COPP is resetting. Do not send TruMedia params to Q6\n",
+			__func__);
+		return -EINVAL;
+	}
+
 	switch (srs_tech_id) {
 	case SRS_ID_GLOBAL: {
 		struct srs_trumedia_params_GLOBAL *glb_params = NULL;
